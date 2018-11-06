df84e575881587ef9b80694c5eee030d5560d7c5
athiels <arne.thiels@gmail.com>
2018-11-06 16:41:06 +0100

    manifest update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,13 @@
-self.addEventListener('fetch', function(e){
-
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          '/img/background.jpg',
+          '/img/SFL-logo.png',
+          'index.html'
+        ]
+      );
+    })
+  );
 });
